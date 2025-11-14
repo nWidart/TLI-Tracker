711f3bcb22652a48894894f87a146d4b0fd04a28
nwidart <n.widart@gmail.com>
2025-11-14 08:13:56 +0100

    Updating entry point

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import py2exe
 options = {
     'py2exe': {
-        'includes': ['win32gui', 'win32process', 'tkinter', 'psutil', 're', 'json']
+        'includes': ['win32gui', 'win32process', 'win32api', 'tkinter', 'psutil', 're', 'json']
     }
 }
 
@@ -10,6 +10,19 @@ setup(
     version="0.0.1a4",
     options=options,
     description="TorchFurry Torchlight Income Statistics Tool - English Version",
-    console=['index_english.py'],
-    data_files=[('',['full_table.json',"update_log.txt","instructions.txt"])]
+    console=['index.py'],
+    data_files=[
+        ('', [
+            'full_table.json',
+            'instructions.txt',
+            'price.json',
+            'id_table.json',
+            'en_id_table.json',
+            'id_table.conf',
+            'en_id_table.conf',
+            'config.json',
+            'translation_mapping.json',
+            'drops.txt'
+        ])
+    ]
 )
